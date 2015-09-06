061148e88d7810b513da4c98cffe0dd9ee89e376
Michael de Lang <kingoipo@gmail.com>
2015-09-06 14:28:43 +0200

    Fix create symlink script

--- IdleLandsRedux.GameLogic.Tests/create_assets_symlink.sh
+++ IdleLandsRedux.GameLogic.Tests/create_assets_symlink.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-if [ ! -d "bin/Debug/assets" ]; then
-  ln -sv ../../../assets bin/Debug/assets
-  exit $?
-else
-  exit 0
+if [ -d "bin/Debug" ]; then
+  if [ ! -d "bin/Debug/assets" ]; then
+    ln -sv ../../../assets bin/Debug/assets
+  fi
 fi
+
+if [ -d "bin/Release" ]; then
+  if [ ! -d "bin/Release/assets" ]; then
+    ln -sv ../../../assets bin/Release/assets
+  fi
+fi
+
+exit 0
